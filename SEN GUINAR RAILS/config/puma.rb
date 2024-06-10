535886833a80213389821ce5c0fdc73946dbcf11
Babacargueye3101 <68257055+Babacargueye3101@users.noreply.github.com>
2024-06-10 19:33:09 +0000

    Update puma.rb

--- SEN GUINAR RAILS/config/puma.rb
+++ SEN GUINAR RAILS/config/puma.rb
@@ -1,15 +1,23 @@
-# config/puma.rb
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+# Nombre de threads minimum et maximum par worker
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
+# Définir le port par défaut
+port        ENV.fetch('PORT') { 3000 }
+
+# Définir l'environnement d'exécution
+environment ENV.fetch('RAILS_ENV') { 'development' }
+
+# Définir le nombre de workers
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+
+# Précharger l'application avant de forker les workers pour utiliser la copie en écriture
 preload_app!
 
-rackup DefaultRackup
-port ENV['PORT'] || 3000
-environment ENV['RACK_ENV'] || 'development'
-
 on_worker_boot do
-  # Spécifiez ici les configurations nécessaires pour les workers
+  # Code à exécuter lorsque le worker démarre
   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 end
+
+# Autoriser Puma à se redémarrer avec `rails restart` command
+plugin :tmp_restart
